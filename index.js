63be06098fe991a71a112fdec4b0949382c384b8
Marlon Munoz <marlon@marlonshade.com>
2024-06-19 20:43:10 -0400

    complete obj lab

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Write your solution in this file!
+const employee = {
+    name: "Marlon",
+    streetAddress: "NYC7"
+};
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return {
+        ...employee,
+        [key]: value,
+    };
+};
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+
+};
+
+function deleteFromEmployeeByKey(employee,key) {
+    const updatedEmployee = {...employee};
+    delete updatedEmployee[key];
+    return updatedEmployee;   
+};
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+};
